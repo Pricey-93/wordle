781b9359a1b4935bffa049935c7f5b504e45abec
Dan <danieljamieprice@gmail.com>
2022-02-03 23:18:17 +0000

    initial class setup

---
+++ wordle/src/Controller.java
@@ -0,0 +1,11 @@
+
+public class Controller {
+
+    private final Model model;
+    private final View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+}
---
+++ wordle/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    Model model =new Model();
+    View view = new View();
+    Controller controller = new Controller(model, view);
+}
